b7418c787b06ab2bc699bfb5c649ea1e81c0929e
nimrahbatool7 <nimrahbatool522@gmail.com>
2026-08-04 10:29:47 +0500

    root and health endpoints

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const express = require("express");
+console.log("this is the main task file ");
 
+const express = require('express');
 const app = express();
-
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send("fikar makawa!");
-});
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+app.get('/', (req,res)=>
+{
+    res.send("this is my new app");
+})
+//adding an endpoint get
+app.get('/', (req, res) =>
+{
+    return res.json({
+        name : "Task API",
+        version : "1.0",
+        endpoints : ["/tasks"],
+    })
+})
+
+app.get("/health", (req, res) =>
+{
+    return res.json({
+        "status" : "ok",
+    })
+})
+
+app.listen(PORT, ()=>
+{
+    console.log(`this is running on port http://localhost:${PORT}`)
+})
