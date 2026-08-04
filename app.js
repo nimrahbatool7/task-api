3e7291d8c5dd9d63016325ccd80b2a3709d6b5eb
nimrahbatool7 <nimrahbatool522@gmail.com>
2026-08-04 11:53:02 +0500

    read endpoints with 404

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ console.log("this is the main task file ");
 
 const express = require('express');
 const app = express();
+const tasks = require('./task_List.js')
 const PORT = 3000;
 
-
-app.get('/', (req,res)=>
-{
-    res.send("this is my new app");
-})
 //adding an endpoint get
 app.get('/', (req, res) =>
 {
@@ -25,6 +21,33 @@ app.get("/health", (req, res) =>
         "status" : "ok",
     })
 })
+
+// Stage 2 — Read: list and single task 
+
+// getting task file 
+app.get('/tasks', (req, res) =>
+{
+    return res.json(tasks);
+})
+// returning task with id 
+app.get('/tasks/:id', (req, res) =>
+{
+    const task_id = Number(req.params.id);
+    const Task = tasks.find((task) => task.id === task_id);
+    if(Task)
+    {
+        return res.json(Task);
+    }
+
+    
+    else
+    {
+        return res.status(404).json({
+            error:`task ${task_id} not found`,
+        });
+    }
+})
+
 
 app.listen(PORT, ()=>
 {
---
+++ task_List.js
@@ -0,0 +1,19 @@
+const tasks= [
+    task1= {
+        id:1,
+        title:'study',
+        done:false,
+    },
+    {
+        id:2,
+        title:'exercise',
+        done:false,
+    },
+    {
+        id:3,
+        title:'read',
+        done:false,
+    }
+];
+
+module.exports = tasks;
